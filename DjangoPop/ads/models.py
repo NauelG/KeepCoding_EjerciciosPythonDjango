0025249e11b55969d3288fa8303236d38520c84e
NauelG <g.nauel@gmail.com>
2018-11-06 22:27:54 +0100

    Add app

--- DjangoPop/ads/models.py
+++ DjangoPop/ads/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Ad(models.Model):
+
+    PUBLISHED = 'PUB'
+    RESERVED = 'RES'
+    SOLD = 'SLD'
+
+    STATUS = (
+        (PUBLISHED, 'Published'),
+        (RESERVED, 'Reserved'),
+        (SOLD, 'Sold')
+    )
+
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.FileField()
+    status = models.CharField(max_length=3, choices=STATUS)
+
+    def __str__(self):
+        return '{0} ({1})'.format(self.name, self.get_status_display())
--- DjangoPop/ads/views.py
+++ DjangoPop/ads/views.py
@@ -2,4 +2,6 @@ from django.http.response import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('Hello World')
+    name = request.GET.get('name', 'Unknown user')
+    age = request.GET.get('age', 'Unknown age')
+    return HttpResponse('Hello {0}! {0} is {1}'.format(name, age))
