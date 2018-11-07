2970620a25d9c179a30cfd1cc69fa6a774be2a0b
NauelG <g.nauel@gmail.com>
2018-11-07 18:32:56 +0100

    Añadimos campos de fecha y ordenamos por modificacion

--- DjangoPop/ads/views.py
+++ DjangoPop/ads/views.py
@@ -4,8 +4,8 @@ from ads.models import Ad
 
 def home(request):
     # 1) Obtener los anuncios de la BBDD
-    ads_list = Ad.objects.filter(status=Ad.PUBLISHED)
-
+    published_ads = Ad.objects.filter(status=Ad.PUBLISHED).order_by('-last_modification')
+    ads_list = published_ads[:4]
     # 2) Pasar los anuncios a la plantilla para que los renderize en HTML
     contex = {'ads': ads_list}
     return render(request, 'ads/home.html', contex)
