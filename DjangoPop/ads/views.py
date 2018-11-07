2e77fef5154ff9dd340182444664ca71fbbae91d
NauelG <g.nauel@gmail.com>
2018-11-07 17:58:56 +0100

    Filtrando datos

--- DjangoPop/ads/views.py
+++ DjangoPop/ads/views.py
@@ -4,7 +4,7 @@ from ads.models import Ad
 
 def home(request):
     # 1) Obtener los anuncios de la BBDD
-    ads_list = Ad.objects.all()
+    ads_list = Ad.objects.filter(status=Ad.PUBLISHED)
 
     # 2) Pasar los anuncios a la plantilla para que los renderize en HTML
     contex = {'ads': ads_list}
