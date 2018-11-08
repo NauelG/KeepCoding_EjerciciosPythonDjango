e6e90758fb892cbcb5821c537efedd11b8990ae6
NauelG <g.nauel@gmail.com>
2018-11-08 21:45:56 +0100

    Optimizando la qt de querys con joins del ORM (select_related)

--- DjangoPop/ads/views.py
+++ DjangoPop/ads/views.py
@@ -5,7 +5,7 @@ from ads.models import Ad
 
 def home(request):
     # 1) Obtener los anuncios de la BBDD
-    published_ads = Ad.objects.filter(status=Ad.PUBLISHED).order_by('-last_modification')
+    published_ads = Ad.objects.select_related('owner').filter(status=Ad.PUBLISHED).order_by('-last_modification')
     ads_list = published_ads[:4]
     # 2) Pasar los anuncios a la plantilla para que los renderize en HTML
     contex = {'ads': ads_list}
@@ -15,7 +15,7 @@ def home(request):
 def ad_detail(request, ad_pk):
     # 1) Obtener el ad de la BBDD
     try:
-        ad = Ad.objects.get(pk=ad_pk)
+        ad = Ad.objects.get(pk=ad_pk).select_related('owner')
         context = {'ad': ad}
         return render(request, 'ads/ad_detail.html', context)
     except Ad.DoesNotExist:
