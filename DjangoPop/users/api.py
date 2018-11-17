0e3eb733b48f9629628bf29defedb6fd665a981b
NauelG <g.nauel@gmail.com>
2018-11-17 19:49:42 +0100

    listado con diferentes datos que el detalle con diferentes serializadores. Save completado

--- DjangoPop/users/api.py
+++ DjangoPop/users/api.py
@@ -1,17 +1,26 @@
 from django.contrib.auth.models import User
+from rest_framework import status
+
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserListSerializer
 
 
 class UsersListAPIView(APIView):
 
     def get(self, request):
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        serializer = UserListSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class UserDetailAPIView(APIView):
